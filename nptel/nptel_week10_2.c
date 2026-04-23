5bf2e2e7029a32f037bc30b83fe8b473b0aadc3c
dhvqn1 <dhvaniiyer@outlook.com>
2026-04-23 12:27:22 +0530

    Create nptel_week10_2.c

---
+++ nptel/nptel_week10_2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[3][3], i, j;
+    int det;
+
+    for(i=0;i<3;i++)
+        for(j=0;j<3;j++)
+            scanf("%d",&a[i][j]);
+
+    det =
+        a[0][0]*(a[1][1]*a[2][2] - a[1][2]*a[2][1]) -
+        a[0][1]*(a[1][0]*a[2][2] - a[1][2]*a[2][0]) +
+        a[0][2]*(a[1][0]*a[2][1] - a[1][1]*a[2][0]);
+
+    if(det == 0)
+        printf("The given matrix is not invertible");
+    else
+        printf("The given matrix is invertible");
+
+    return 0;
+}
