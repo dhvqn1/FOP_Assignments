8975bf3ee591410cc6a66c4569721a0133c8bd78
dhvqn1 <dhvaniiyer@outlook.com>
2026-04-23 11:36:04 +0530

    Create nptel_week6_1.c

---
+++ nptel/nptel_week6_1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,i,largest;
+    int arr[100];
+
+    scanf("%d",&n);
+
+    for(i=0;i<n;i++)
+        scanf("%d",&arr[i]);
+
+    largest = arr[0];
+
+    for(i=1;i<n;i++)
+    {
+        if(arr[i] > largest)
+            largest = arr[i];
+    }
+
+    printf("Largest element = %d", largest);
+
+    return 0;
+}
