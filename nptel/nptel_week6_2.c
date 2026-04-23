1148287ff2cd69cf792733a31e2bc793f42be061
dhvqn1 <dhvaniiyer@outlook.com>
2026-04-23 11:37:05 +0530

    Create nptel_week6_2.c

---
+++ nptel/nptel_week6_2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[20], n, i, j, temp;
+
+    scanf("%d",&n);
+
+    for(i=0;i<n;i++)
+        scanf("%d",&arr[i]);
+
+    i = 0;
+    j = n - 1;
+
+    while(i < j)
+    {
+        temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+
+        i++;
+        j--;
+    }
+
+    for(i=0;i<n;i++)
+        printf("%d\n",arr[i]);
+
+    return 0;
+}
